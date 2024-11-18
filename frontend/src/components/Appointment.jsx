6756fe729cbbd36d2aa1de365af2988233fe4396
Cham <chamboquilon1@gmail.com>
2024-11-18 23:15:20 +0800

    cham-v1
    partial design for appointment form

--- frontend/src/components/Appointment.jsx
+++ frontend/src/components/Appointment.jsx
@@ -3,58 +3,54 @@ import "./Appointment.css";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css"; // Default DatePicker styles
 
-
-
 // const handleTimeChange = (event) => {
-    //     setSelectedTime(event.target.value);
+//     setSelectedTime(event.target.value);
 
-    // const handleSubmit = () => {
-    //     if (selectedDate) {
-    //         const combinedDate = new Date(selectedDate);
-    //         const [hours, minutes] = selectedTime.split(":").map(Number);
-    //         combinedDate.setHours(hours, minutes);
-    //         console.log("Selected Date and Time:", combinedDate);
-    //     }
-    // };
-
+// const handleSubmit = () => {
+//     if (selectedDate) {
+//         const combinedDate = new Date(selectedDate);
+//         const [hours, minutes] = selectedTime.split(":").map(Number);
+//         combinedDate.setHours(hours, minutes);
+//         console.log("Selected Date and Time:", combinedDate);
+//     }
+// };
 
 function Appointment() {
-    const [selectedBirthDate, setSelectedBirthDate] = useState('');
-    const [selectedAppointmentDate, setAppointmentDate] = useState('');
+    const [selectedBirthDate, setSelectedBirthDate] = useState("");
+    const [selectedAppointmentDate, setAppointmentDate] = useState("");
     const [firstName, setFirstName] = useState("");
     const [lastName, setLastName] = useState("");
     const [middleName, setMiddleName] = useState("");
     const [Suffix, setSuffix] = useState("");
-    const [Age, setAge] = useState('');
-    const [streetBuilding, setStreetBuilding] = useState('');
-    const [Barangay, setBarangay] = useState('');
-    const [City, setCity] = useState('');
-    const [contactNumber, setContactNumber] = useState('');
-    const [selectedBranch, setBranch] = useState('');
-    const services = ['Service 1', 'Service 2', 'Service 3'];
-    const [selectedService, setSelectedService] = useState('');
+    const [Age, setAge] = useState("");
+    const [streetBuilding, setStreetBuilding] = useState("");
+    const [Barangay, setBarangay] = useState("");
+    const [City, setCity] = useState("");
+    const [contactNumber, setContactNumber] = useState("");
+    const [selectedBranch, setBranch] = useState("");
+    const services = ["Service 1", "Service 2", "Service 3"];
+    const [selectedService, setSelectedService] = useState("");
 
     // Handle the change event for the dropdown
-    
+
     const handleBirthDate = (date) => {
-        if(!date){
-            setSelectedBirthDate('');
-            setAge('');
-        }else{
+        if (!date) {
+            setSelectedBirthDate("");
+            setAge("");
+        } else {
             setSelectedBirthDate(date.toLocaleDateString());
             const currentYear = new Date().getFullYear();
             setAge(currentYear - date.getFullYear());
         }
-        
     };
 
-    const handleAppointDate = (date) =>{
-        if(!date){
+    const handleAppointDate = (date) => {
+        if (!date) {
             setAppointmentDate(null);
-        }else{
+        } else {
             setAppointmentDate(date);
         }
-    }
+    };
 
     const handleFirstName = (e) => {
         setFirstName(e.target.value); // Update state with input value
@@ -78,7 +74,6 @@ function Appointment() {
 
     const handleBarangay = (event) => {
         setBarangay(event.target.value);
-        
     };
 
     const handleCity = (event) => {
@@ -89,21 +84,18 @@ function Appointment() {
         setContactNumber(event.target.value);
     };
 
-    const handleBranch = (branchName) =>{
+    const handleBranch = (branchName) => {
         setBranch(branchName);
-    }
+    };
 
     const handleServiceChange = (event) => {
         setSelectedService(event.target.value);
     };
-    
-    const handleSubmit = (event) => {
 
+    const handleSubmit = (event) => {
         console.log("Form submitted");
         // Handle further logic like form submission to API/database, etc.
-      };
-
-
+    };
 
     console.log(`
         ${firstName} ${middleName} ${lastName} ${Suffix}
@@ -134,82 +126,91 @@ function Appointment() {
                                     <input
                                         type="text"
                                         placeholder="First Name"
-                                        value={firstName} 
+                                        className="firstname"
+                                        value={firstName}
                                         onChange={handleFirstName}
                                     />
-                            
+
                                     <input
-                                        type="text" 
+                                        type="text"
                                         placeholder="Suffix"
+                                        className="suffix"
                                         value={Suffix}
                                         onChange={handleSuffix}
                                     />
                                 </form>
                             </div>
+                            <div className="appointment-div2">
+                                <form action="">
+                                    <input
+                                        type="text"
+                                        placeholder="Middle Name"
+                                        value={middleName}
+                                        onChange={handleMiddleName}
+                                    />
 
-                            <form action="">
-                                <input 
-                                    type="text" 
-                                    placeholder="Middle Name"
-                                    value={middleName}
-                                    onChange={handleMiddleName}
-                                />
-                            
-                                <input 
-                                    type="text" 
-                                    placeholder="Last Name"
-                                    value={lastName}
-                                    onChange={handleLastName}
-                                />
-                           
-        
-                            <DatePicker
-                                selected={selectedBirthDate}
-                                onChange={handleBirthDate}
-                                placeholderText="Birth Date"
-                                showMonthDropdown
-                                showYearDropdown
-                                dropdownMode="select"
-                                disabled={false}
-                            />
-                            {/* <h3>Contact Information</h3> */}
+                                    <input
+                                        type="text"
+                                        placeholder="Last Name"
+                                        value={lastName}
+                                        onChange={handleLastName}
+                                    />
 
-                            
-                                <input
-                                    type="text"
-                                    placeholder="Street / Building"
-                                    value={streetBuilding}
-                                    onChange={handleStreetBuilding}
-                                />
-                            
-                                <input 
-                                    type="text" 
-                                    placeholder="Barangay"
-                                    value={Barangay}
-                                    onChange={handleBarangay} 
-                                />
-                           
-                                <input 
-                                    type="text" 
-                                    placeholder="City" 
-                                    value={City}
-                                    onChange={handleCity}
-                                />
-                            
-                                <input
-                                    type="text"
-                                    placeholder="Contact Number"
-                                    value={contactNumber}
-                                    onChange={handleContactNumber}
-                                />
-                            </form>
+                                    <DatePicker
+                                        selected={selectedBirthDate}
+                                        onChange={handleBirthDate}
+                                        placeholderText="Birth Date"
+                                        showMonthDropdown
+                                        showYearDropdown
+                                        dropdownMode="select"
+                                        disabled={false}
+                                    />
+                                    {/* <h3>Contact Information</h3> */}
+
+                                    <input
+                                        type="text"
+                                        placeholder="Street / Building"
+                                        value={streetBuilding}
+                                        onChange={handleStreetBuilding}
+                                    />
+
+                                    <input
+                                        type="text"
+                                        placeholder="Barangay"
+                                        value={Barangay}
+                                        onChange={handleBarangay}
+                                    />
+
+                                    <input
+                                        type="text"
+                                        placeholder="City"
+                                        value={City}
+                                        onChange={handleCity}
+                                    />
+
+                                    <input
+                                        type="text"
+                                        placeholder="Contact Number"
+                                        value={contactNumber}
+                                        onChange={handleContactNumber}
+                                    />
+                                </form>
+                            </div>
                         </div>
 
                         <div className="appointment-right">
-                            <h3>Branch</h3>
-                            <button onClick={() => handleBranch('Butuan')}>Butuan</button>
-                            <button onClick={() => handleBranch('Gingoog')}>Gingoog</button>
-                            <h3>Appointment Date</h3>
+                            <h3 className="appointment-branch">Branch</h3>
+                            <div className="branch-button">
+                                <button onClick={() => handleBranch("Butuan")}>
+                                    Butuan
+                                </button>
+                                <button onClick={() => handleBranch("Gingoog")}>
+                                    Gingoog
+                                </button>
+                            </div>
+                            <h3 className="appointment-date">
+                                Appointment Date
+                            </h3>
 
                             <DatePicker
                                 placeholderText="MM/DD/YYYY"
@@ -221,25 +222,37 @@ function Appointment() {
                                 maxTime={new Date().setHours(18, 0, 0, 0)} // 6 PM
                             />
 
-                            <h3>Purpose of Visitation</h3>
+                            <h3 className="appointment-services">
+                                Purpose of Visitation
+                            </h3>
 
                             <form>
-                                <label htmlFor="services">Select Service:</label>
+                                <label htmlFor="services">
+                                    {/* Select Service: */}
+                                </label>
                                 <select
                                     id="services"
+                                    className="appointment-services-menu"
                                     value={selectedService} // Bind the selected service to state
                                     onChange={handleServiceChange} // Handle dropdown change
                                 >
-                                    <option value="">--Select a Service--</option>
-                                    {services.map((service, index) => (
-                                    <option key={index} value={service}>
-                                        {service}
+                                    <option value="">
+                                        --Select a Service--
                                     </option>
+                                    {services.map((service, index) => (
+                                        <option key={index} value={service}>
+                                            {service}
+                                        </option>
                                     ))}
                                 </select>
-                                </form>
+                            </form>
 
-                            <button onClick={handleSubmit}>SET APPOINTMENT</button>
+                            <button
+                                onClick={handleSubmit}
+                                className="set-appointment-button"
+                            >
+                                SET APPOINTMENT
+                            </button>
                         </div>
                     </div>
                 </div>
