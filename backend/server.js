1d5b276449e6ac7f0ab97885d0c8b3546e10b363
Kirisukyoto <christian.isiderio@carsu.edu.ph>
2024-11-30 12:59:39 +0000

    FB DB setup

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,7 @@
-// backend/server.js
-
 const express = require("express");
 const admin = require("firebase-admin");
 const cors = require("cors");
+require("dotenv").config();  // Make sure to load the .env file
 
 const app = express();
 const port = 5000;
@@ -11,9 +10,8 @@ const port = 5000;
 app.use(cors());
 app.use(express.json()); // For parsing application/json
 
-// Initialize Firebase Admin SDK
-const serviceAccount = require("./appointmentServiceKey.json"); // Replace with path to your service account file
-
+// Initialize Firebase Admin SDK using the service account key from environment variables
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://appointment-hub-76926-default-rtdb.firebaseio.com/", // Use your Firebase Realtime Database URL
