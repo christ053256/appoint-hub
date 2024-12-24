315931da835ca9267cf38c8741ea4d65b09b3245
Piattos32724 <cj.pesical@carsu.edu.ph>
2024-12-24 02:10:19 +0000

    About Page Complete

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -4,9 +4,15 @@ import "./About.css";
 function About() {
     return (
         <div className="about-container">
-            <div> About</div>
+            <div>
+                <h1>About Us</h1>
+                <p>
+                    At 8Care, we are committed to providing exceptional dental care in a comfortable and welcoming environment. Our team of experienced dentist and compassionate staff work together to deliver personalized treatment plans tailored to meet the unique needs of every patient. Whether you're visiting us for routine cleanings, advanced restorative work, or cosmetic enhancements, we prioritize your comfort and satisfaction at every step. Using state-of-the-art technology and the latest techniques, we strive to create healthy, beautiful smiles that last a lifetime. Your oral health is our top priority, and we are dedicated to building long-term relationships with our patients based on trust, respect, and outstanding care.
+                </p>
+            </div>
         </div>
     );
 }
 
 export default About;
+
