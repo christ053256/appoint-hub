d192efb8849f4575afa4a63b97ca6e7b657f4aad
Piattos32724 <cj.pesical@carsu.edu.ph>
2024-12-24 02:51:59 +0000

    sadasdasd

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -3,14 +3,14 @@ import "./About.css";
 
 function About() {
     return (
-        <div className="home-container">
-            <div className="home-left">
+        <div className="about-container">
+            <div className="about-left">
                 <p className="">
-                    <span className="home-d">About Us</span>
+                    <span className="about-d">About Us</span>
                 </p>
-                <p className="home-p">At 8Care, we are committed to providing exceptional dental care in a comfortable and welcoming environment. Our team of experienced dentist and compassionate staff work together to deliver personalized treatment plans tailored to meet the unique needs of every patient. Whether you're visiting us for routine cleanings, advanced restorative work, or cosmetic enhancements, we prioritize your comfort and satisfaction at every step. Using state-of-the-art technology and the latest techniques, we strive to create healthy, beautiful smiles that last a lifetime. Your oral health is our top priority, and we are dedicated to building long-term relationships with our patients based on trust, respect, and outstanding care.</p>
+                <p className="about-p">At 8Care, we are committed to providing exceptional dental care in a comfortable and welcoming environment. Our team of experienced dentist and compassionate staff work together to deliver personalized treatment plans tailored to meet the unique needs of every patient. Whether you're visiting us for routine cleanings, advanced restorative work, or cosmetic enhancements, we prioritize your comfort and satisfaction at every step. Using state-of-the-art technology and the latest techniques, we strive to create healthy, beautiful smiles that last a lifetime. Your oral health is our top priority, and we are dedicated to building long-term relationships with our patients based on trust, respect, and outstanding care.</p>
             </div>
-            <div className="home-right">{/* BUTTON FOR ADMIN */}</div>
+            <div className="about-right">{/* BUTTON FOR ADMIN */}</div>
         </div>
         /*<div className="about-container">
             <div>
