24df84c3437f919d6c9afc7c5f7b203140a25c4c
Piattos32724 <cj.pesical@carsu.edu.ph>
2024-12-24 02:57:05 +0000

    asdasdasd

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -4,22 +4,16 @@ import "./About.css";
 function About() {
     return (
         <div className="about-container">
-            <div className="about-left">
+            <div className="about-title">
                 <p className="">
                     <span className="about-d">About Us</span>
                 </p>
+            </div>
+            <div className="about-paragraph">
                 <p className="about-p">At 8Care, we are committed to providing exceptional dental care in a comfortable and welcoming environment. Our team of experienced dentist and compassionate staff work together to deliver personalized treatment plans tailored to meet the unique needs of every patient. Whether you're visiting us for routine cleanings, advanced restorative work, or cosmetic enhancements, we prioritize your comfort and satisfaction at every step. Using state-of-the-art technology and the latest techniques, we strive to create healthy, beautiful smiles that last a lifetime. Your oral health is our top priority, and we are dedicated to building long-term relationships with our patients based on trust, respect, and outstanding care.</p>
             </div>
             <div className="about-right">{/* BUTTON FOR ADMIN */}</div>
         </div>
-        /*<div className="about-container">
-            <div>
-                <h1>About Us</h1>
-                <p>
-                    At 8Care, we are committed to providing exceptional dental care in a comfortable and welcoming environment. Our team of experienced dentist and compassionate staff work together to deliver personalized treatment plans tailored to meet the unique needs of every patient. Whether you're visiting us for routine cleanings, advanced restorative work, or cosmetic enhancements, we prioritize your comfort and satisfaction at every step. Using state-of-the-art technology and the latest techniques, we strive to create healthy, beautiful smiles that last a lifetime. Your oral health is our top priority, and we are dedicated to building long-term relationships with our patients based on trust, respect, and outstanding care.
-                </p>
-            </div>
-        </div>*/
     );
 }
 
