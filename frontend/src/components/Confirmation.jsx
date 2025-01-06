0426484c64b31e85d7f1e58a6b342de5c3c23442
Cham <122889688+chamborgir@users.noreply.github.com>
2025-01-06 23:47:53 +0800

    final-v5

--- frontend/src/components/Confirmation.jsx
+++ frontend/src/components/Confirmation.jsx
@@ -1,8 +1,29 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
 import "./Confirmation.css";
 
 function Confirmation() {
-    return <div className="confirmation-container ">Confirmation</div>;
+    return (
+        <div className="confirmation-container ">
+            <div className="confirmation-placeholder">
+                <div className="icon-holder">
+                    <div className="circle">
+                        <div class="checkmark"></div>
+                    </div>
+                </div>
+
+                <h1>Success!</h1>
+                <p>You have successfully set an appointment.</p>
+                <p>Please wait for the SMS confirmation.</p>
+                <Link to="/">
+                    <button className="return-button">
+                        Return to Homepage
+                    </button>
+                </Link>
+            </div>
+        </div>
+    );
 }
 
 export default Confirmation;
